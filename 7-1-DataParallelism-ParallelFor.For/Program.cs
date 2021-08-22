1b5111e01158340f9245df749ded90743ae97faf
Vancho Dimitrov <vancho.dimitrov@gmail.com>
2021-08-22 15:59:41 +0200

    Add files via upload

---
+++ 7-1-DataParallelism-ParallelFor.For/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace _7_DataParallelism_ParallelFor.For
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 4, 6, 7, 9, 0, 2, 3, 5, 8 };
+
+            Parallel.For(0, numbers.Length, i => Console.Write(i));
+
+
+            string[] cities = { "New York", "Dublin", "London", "Munich", "Madrid" };
+
+            Parallel.For(0, cities.Length, i => Console.WriteLine(cities[i]));
+
+            var citiesCollection = new List<string> { "New York", "Dublin", "London", "Munich", "Madrid" };
+
+            Parallel.For(0, citiesCollection.Count, i =>
+            {
+                Console.WriteLine(citiesCollection[i]);
+
+            });
+
+        }
+    }
+}
---
+++ 8-DataParallelismForEachComplexType/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace _8_DataParallelismForEachComplexType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+
+            Parallel.ForEach(employee.InsertEmployees(), i =>
+            {
+                Console.WriteLine(i.Id + " " + i.Name);
+            });
+        }
+    }
+
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public List<Employee> employees { get; set; } = new();
+
+        public List<Employee> InsertEmployees()
+        {
+            employees = new List<Employee>()
+            {
+                new Employee{ Id=1,Name="Name1"},
+                new Employee{ Id=2,Name="Name2"},
+                new Employee{ Id=3,Name="Name3"},
+                new Employee{ Id=4,Name="Name4"},
+                new Employee{ Id=5,Name="Name5"},
+            };
+
+            return employees;
+        }
+    }
+}
