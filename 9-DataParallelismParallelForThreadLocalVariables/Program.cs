8b1144cabd01e162428b47558959b03b56b661d8
Vancho Dimitrov <vancho.dimitrov@gmail.com>
2022-09-13 14:57:35 +0200

    fixes in the 9-DataParallelismParallelForThreadLocalVariables

--- 9-DataParallelismParallelForThreadLocalVariables/Program.cs
+++ 9-DataParallelismParallelForThreadLocalVariables/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace _9_DataParallelismParallelForThreadLocalVariables
 {
@@ -6,7 +9,24 @@ namespace _9_DataParallelismParallelForThreadLocalVariables
     {
         static void Main(string[] args)
         {
+            List<int> nums = new List<int>();
 
+            for (int i = 0; i <= 10000; i++)
+            {
+                nums.Add(i);
+            }
+
+            int refSum = 0;
+
+            Parallel.For<int>(0, nums.Count, () => 0, (i, loop, sum) =>
+            {
+                sum += nums[i];
+                return sum;
+            },
+                (x) => Interlocked.Add(ref refSum, x)
+                );
+
+            Console.WriteLine("The SUM is {0:N0}", refSum);
         }
     }
 }
